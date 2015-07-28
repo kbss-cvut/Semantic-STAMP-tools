1e95c9db89b175e05b028b9c504faf6f4fff0843
Martin Ledvinka <ledvima1@fel.cvut.cz>
2015-07-28 12:50:48 +0200

    Added hashCode and equals to event type, here we can use URI, because it is not auto-generated and is expected to be present.

--- inbas-reporting-tool/src/main/java/cz/cvut/kbss/inbas/audit/model/EventType.java
+++ inbas-reporting-tool/src/main/java/cz/cvut/kbss/inbas/audit/model/EventType.java
@@ -54,6 +54,25 @@ public class EventType {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EventType eventType = (EventType) o;
+
+        if (id != null ? !id.equals(eventType.id) : eventType.id != null) return false;
+        return !(name != null ? !name.equals(eventType.name) : eventType.name != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "EventType{" +
                 "id=" + id +
