a06d634aedbbdc2c8216082054252d860049680e
Martin Ledvinka <ledvima1@fel.cvut.cz>
2015-09-24 16:57:04 +0200

    Improved styling and event handling of the diagram component.

--- inbas-reporting-tool/src/main/webapp/js/components/investigation/Factors.js
+++ inbas-reporting-tool/src/main/webapp/js/components/investigation/Factors.js
@@ -6,6 +6,15 @@ var Utils = require('../../utils/Utils');
 
 var DATE_FORMAT = '%d-%m-%y %h:%i %A';
 
+function lightboxHeader(startDate, endDate, event) {
+    var text = event.text && !event.$new ? event.text.substr(0, 70) : 'New Event';
+    return gantt.templates.task_date(event.start_date) + "&nbsp;" + text;
+}
+
+function linkAdded(id, link) {
+    console.log(id);
+}
+
 var Factors = React.createClass({
 
     propTypes: {
@@ -15,25 +24,31 @@ var Factors = React.createClass({
     componentDidMount: function () {
         this.configureGantt();
         gantt.init('factors_gantt');
+        gantt.attachEvent('onTaskCreated', function (task) {
+            task.text = '';
+            return true;
+        });
         this.addOccurrenceEvent();
     },
 
     configureGantt: function () {
         gantt.config.columns = [
-            {name: "text", label: "Event", width: "*", tree: true},
-            {name: "start_date", label: "Start time", align: "center"},
-            {name: "duration", label: "Duration", align: "center"},
-            {name: "add", label: "", width: 44}
+            {name: 'text', label: 'Event', width: '*', tree: true},
+            {name: 'start_date', label: 'Start time', width: '*', align: 'center'},
+            {name: 'add', label: '', width: 44}
         ];
         gantt.config.scale_unit = 'minute';
         gantt.config.api_date = DATE_FORMAT;
         gantt.config.date_scale = '%h:%i %A';
+        gantt.config.date_grid = DATE_FORMAT;
         gantt.config.fit_tasks = true;
         gantt.config.duration_unit = 'minute';
         gantt.config.duration_step = 1;
         gantt.config.scroll_on_click = true;
-        //gantt.config.drag_resize = true;
+        gantt.config.show_errors = false;   // Get rid of errors in case the grid has to resize
         gantt.config.drag_progress = false;
+        gantt.templates.lightbox_header = lightboxHeader;
+        gantt.attachEvent('onAfterLinkAdd', linkAdded);
     },
 
     addOccurrenceEvent: function () {
@@ -48,13 +63,8 @@ var Factors = React.createClass({
     },
 
     render: function () {
-        var style = {
-            width: '100%',
-            height: '400px'
-        };
-        return (<div id='factors_gantt' style={style}></div>);
+        return (<div id='factors_gantt' className='factors-gantt'/>);
     }
 });
 
 module.exports = Factors;
-
