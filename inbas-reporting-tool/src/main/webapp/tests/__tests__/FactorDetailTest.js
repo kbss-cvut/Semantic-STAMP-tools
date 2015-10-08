56bc431a0f4feda55a2fa3bfab9c97b09f166db1
Martin Ledvinka <ledvima1@fel.cvut.cz>
2015-10-08 14:00:14 +0200

    Some tests for the factor detail dialog.

---
+++ inbas-reporting-tool/src/main/webapp/tests/__tests__/FactorDetailTest.js
@@ -0,0 +1,86 @@
+'use strict';
+
+jest.dontMock('../../js/components/investigation/FactorDetail');
+jest.dontMock('../../js/components/reports/wizard/event-type/EventTypeWizardSelector');
+
+describe('Tests of the factor dialog', function () {
+
+    var React = require('react/addons'),
+        TestUtils = React.addons.TestUtils,
+        FactorDetail = require('../../js/components/investigation/FactorDetail'),
+        assign = require('object-assign'),
+        callbacks = {
+            onSave: function () {
+            },
+            onClose: function () {
+            },
+            onDelete: function () {
+            }
+        },
+        gantt = {
+            calculateEndDate: function () {
+                return new Date();
+            },
+            config: {
+                duration_unit: 'minute'
+            }
+        },
+        factor;
+
+    beforeEach(function () {
+        window.gantt = gantt;
+        factor = {
+            id: 1,
+            text: 'Test',
+            start_date: new Date(),
+            duration: 1
+        };
+    });
+
+    it('Updates factor with new values upon save', function () {
+        var detail, newDuration = 10,
+            eventType = {
+                name: 'Runway Incursion',
+                id: 'http://incursion'
+            };
+        spyOn(callbacks, 'onSave');
+        spyOn(gantt, 'calculateEndDate').andCallThrough();
+        detail = TestUtils.renderIntoDocument(<FactorDetail scale='minute' factor={factor} onSave={callbacks.onSave}
+                                                            onClose={callbacks.onClose}
+                                                            onDelete={callbacks.onDelete}/>);
+        detail.onDurationSet({target: {value: newDuration}});
+        detail.onEventTypeChange(eventType);
+        detail.onSave();
+
+        expect(gantt.calculateEndDate).toHaveBeenCalledWith(factor.start_date, newDuration, 'minute');
+        expect(factor.end_date).toBeDefined();
+        expect(callbacks.onSave).toHaveBeenCalled();
+    });
+
+    it('Preserves factor state until save is called', function () {
+        var detail, newDuration = 10,
+            eventType = {
+                name: 'Runway Incursion',
+                id: 'http://incursion'
+            },
+            origFactor = assign({}, factor),
+            details = {
+                intruder: {},
+                lvp: 'none',
+                location: 'LKPR31'
+            };
+        detail = TestUtils.renderIntoDocument(<FactorDetail scale='minute' factor={factor} onSave={callbacks.onSave}
+                                                            onClose={callbacks.onClose}
+                                                            onDelete={callbacks.onDelete}/>);
+        detail.onDurationSet({target: {value: newDuration}});
+        detail.onEventTypeChange(eventType);
+        detail.onUpdateFactorDetails({statement: details}, function () {
+        });
+
+        expect(factor).toEqual(origFactor);
+        detail.onSave();
+        expect(factor.end_date).toBeDefined();
+        expect(factor.details).toEqual(details);
+        expect(factor.eventType).toEqual(eventType);
+    });
+});
