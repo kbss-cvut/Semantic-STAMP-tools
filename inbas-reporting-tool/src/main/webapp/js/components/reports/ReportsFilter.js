250d757da5338621a039aa75d7135895bb3e57af
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-02-10 17:21:19 +0100

    Implemented report filtering by category + integrated the filters.

--- inbas-reporting-tool/src/main/webapp/js/components/reports/ReportsFilter.js
+++ inbas-reporting-tool/src/main/webapp/js/components/reports/ReportsFilter.js
@@ -5,6 +5,7 @@ var Input = require('../Input');
 var Col = require('react-bootstrap').Col;
 var Row = require('react-bootstrap').Row;
 
+var Constants = require('../../constants/Constants');
 var injectIntl = require('../../utils/injectIntl');
 var I18nMixin = require('../../i18n/I18nMixin');
 
@@ -12,37 +13,69 @@ var ReportsFilter = React.createClass({
     mixins: [I18nMixin],
 
     propTypes: {
-        onFilterChange: React.PropTypes.func.isRequired
+        onFilterChange: React.PropTypes.func.isRequired,
+        reports: React.PropTypes.array
     },
 
     getInitialState: function () {
         return {
-            phase: 'all'
+            'phase': 'all',
+            'occurrenceCategory.id': 'all'
         }
     },
 
     onSelect: function (e) {
-        var phase = e.target.value;
-        this.setState({phase: phase});
-        this.props.onFilterChange({phase: phase});
+        var value = e.target.value;
+        var change = {};
+        change[e.target.name] = value;
+        this.setState(change);
+        this.props.onFilterChange(change);
     },
 
 
     render: function () {
         return (
             <Row className='show-grid filter'>
-                <Col xs={1}><h4>{this.i18n('reports.filter.label')}</h4></Col>
-                <Col xs={3} style={{margin: '3px 0 3px 0'}}>
-                    <Input type='select' label={this.i18n('reports.table-type')} value={this.state.type}
-                           labelClassName='col-xs-1 filter-label'
-                           wrapperClassName='col-xs-7' title={this.i18n('reports.filter.type.tooltip')}
+                <Col xs={2}><h4>{this.i18n('reports.filter.label')}</h4></Col>
+                <Col xs={1}>&nbsp;</Col>
+                <Col xs={4}>&nbsp;</Col>
+                <Col xs={2} style={{margin: '0 0 -15px 0', padding: '3px 3px 0 3px'}}>
+                    <Input type='select' name='occurrenceCategory.id' value={this.state.category}
+                           title={this.i18n('reports.table-classification.tooltip')}
                            onChange={this.onSelect}>
-                        <option value='all'>{this.i18n('reports.filter.type.all')}</option>
-                        <option value='preliminary'>{this.i18n('reports.filter.type.preliminary')}</option>
-                        <option value='investigation'>{this.i18n('investigation.type')}</option>
+                        {this.renderClassificationOptions()}
                     </Input>
                 </Col>
+                <Col xs={1} style={{margin: '0 0 -15px 0', padding: '3px 3px 0 3px'}}>
+                    <Input type='select' name='phase' value={this.state.type}
+                           title={this.i18n('reports.filter.type.tooltip')}
+                           onChange={this.onSelect}>
+                        <option key='type-all' value='all'>{this.i18n('reports.filter.type.all')}</option>
+                        <option key='type-preliminary'
+                                value={Constants.PRELIMINARY_REPORT_PHASE}>{this.i18n('reports.filter.type.preliminary')}</option>
+                        <option key='type-investigation'
+                                value={Constants.INVESTIGATION_REPORT_PHASE}>{this.i18n('investigation.type')}</option>
+                    </Input>
+                </Col>
+                <Col xs={2}>&nbsp;</Col>
             </Row>);
+    },
+
+    renderClassificationOptions: function () {
+        var categories = [],
+            options = [],
+            category,
+            reports = this.props.reports;
+        options.push(<option key='category-all' value='all'>{this.i18n('reports.filter.type.all')}</option>);
+        for (var i = 0, len = reports.length; i < len; i++) {
+            category = reports[i].occurrenceCategory;
+            if (categories.indexOf(category.id) !== -1) {
+                continue;
+            }
+            categories.push(category.id);
+            options.push(<option key={'category-' + category.id} value={category.id}>{category.name}</option>);
+        }
+        return options;
     }
 });
 
--- inbas-reporting-tool/src/main/webapp/tests/__tests__/ReportsControllerTest.js
+++ inbas-reporting-tool/src/main/webapp/tests/__tests__/ReportsControllerTest.js
@@ -12,7 +12,11 @@ describe('ReportsController', function () {
         ReportsController = require('../../js/components/reports/ReportsController'),
         Reports = require('../../js/components/reports/Reports');
 
-    it('shows only reports of the corresponding type when type filter is triggered.', function () {
+    beforeEach(function() {
+        spyOn(Actions, 'loadAllReports');
+    });
+
+    it('shows only reports of the corresponding type when type filter is triggered', function () {
         var controller = Environment.render(<ReportsController />),
             reportsComponent = TestUtils.findRenderedComponentWithType(controller, Reports),
             reports = Generator.generateReports(),
@@ -21,12 +25,84 @@ describe('ReportsController', function () {
         renderedReports = reportsComponent.props.reports;
         expect(renderedReports).toEqual(reports);
 
-        filter = {phase: 'preliminary'};
+        filter = {phase: Constants.PRELIMINARY_REPORT_PHASE};
         controller.onFilterChange(filter);
         renderedReports = reportsComponent.props.reports;
         expect(renderedReports.length).toEqual(Math.ceil(reports.length / 2));
         for (var i = 0, len = renderedReports.length; i < len; i++) {
             expect(renderedReports[i].phase).toEqual(Constants.PRELIMINARY_REPORT_PHASE);
         }
+    });
+
+    it('shows only reports in the corresponding category when category filter is triggered', function () {
+        var controller = Environment.render(<ReportsController />),
+            reportsComponent = TestUtils.findRenderedComponentWithType(controller, Reports),
+            reports = Generator.generateReports(),
+            categories = Generator.getCategories(),
+            renderedReports, filter, matchingReportsCount;
+        controller.onReportsLoaded({action: Actions.loadAllReports, reports: reports});
+        renderedReports = reportsComponent.props.reports;
+        expect(renderedReports).toEqual(reports);
+
+        filter = {'occurrenceCategory.id': categories[Generator.getRandomInt(categories.length)].id};
+        controller.onFilterChange(filter);
+        renderedReports = reportsComponent.props.reports;
+        matchingReportsCount = reports.filter(function (item) {
+            return item.occurrenceCategory.id === filter['occurrenceCategory.id'];
+        }).length;
+        expect(renderedReports.length).toEqual(matchingReportsCount);
+        for (var i = 0, len = renderedReports.length; i < len; i++) {
+            expect(renderedReports[i].occurrenceCategory.id).toEqual(filter['occurrenceCategory.id']);
+        }
+    });
+
+    it('shows only reports matching all filters', function () {
+        var controller = Environment.render(<ReportsController />),
+            reportsComponent = TestUtils.findRenderedComponentWithType(controller, Reports),
+            reports = Generator.generateReports(),
+            renderedReports, filter, sampleReport;
+        controller.onReportsLoaded({action: Actions.loadAllReports, reports: reports});
+        renderedReports = reportsComponent.props.reports;
+        expect(renderedReports).toEqual(reports);
+
+        sampleReport = reports[Generator.getRandomInt(reports.length)];
+        filter = {
+            'phase': sampleReport.phase,
+            'occurrenceCategory.id': sampleReport.occurrenceCategory.id
+        };
+        controller.onFilterChange(filter);
+        renderedReports = reportsComponent.props.reports;
+        expect(renderedReports.length).not.toBeLessThan(1);
+        for (var i = 0, len = renderedReports.length; i < len; i++) {
+            expect(renderedReports[i].phase).toEqual(filter['phase']);
+            expect(renderedReports[i].occurrenceCategory.id).toEqual(filter['occurrenceCategory.id']);
+        }
+    });
+
+    it('shows all reports when all filters are switched to show all', function () {
+        var controller = Environment.render(<ReportsController />),
+            reportsComponent = TestUtils.findRenderedComponentWithType(controller, Reports),
+            reports = Generator.generateReports(),
+            renderedReports, filter, sampleReport;
+        controller.onReportsLoaded({action: Actions.loadAllReports, reports: reports});
+        renderedReports = reportsComponent.props.reports;
+        expect(renderedReports).toEqual(reports);
+
+        sampleReport = reports[Generator.getRandomInt(reports.length)];
+        filter = {
+            'phase': sampleReport.phase,
+            'occurrenceCategory.id': sampleReport.occurrenceCategory.id
+        };
+        controller.onFilterChange(filter);
+        renderedReports = reportsComponent.props.reports;
+        expect(renderedReports.length).toBeLessThan(reports.length);
+
+        filter = {
+            'phase': 'all',
+            'occurrenceCategory.id': 'all'
+        };
+        controller.onFilterChange(filter);
+        renderedReports = reportsComponent.props.reports;
+        expect(renderedReports.length).toEqual(reports.length);
     });
 });
