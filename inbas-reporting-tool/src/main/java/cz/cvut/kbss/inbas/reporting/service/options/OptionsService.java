b917893c45fff237439d59c03f64dfa362eca809
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-04-06 15:25:59 +0200

    OptionsController tests.

--- inbas-reporting-tool/src/main/java/cz/cvut/kbss/inbas/reporting/service/options/OptionsService.java
+++ inbas-reporting-tool/src/main/java/cz/cvut/kbss/inbas/reporting/service/options/OptionsService.java
@@ -1,66 +1,16 @@
 package cz.cvut.kbss.inbas.reporting.service.options;
 
-import cz.cvut.kbss.inbas.reporting.model.reports.OccurrenceSeverity;
-import cz.cvut.kbss.inbas.reporting.model.reports.incursions.LowVisibilityProcedure;
 import cz.cvut.kbss.inbas.reporting.rest.dto.model.RawJson;
-import cz.cvut.kbss.inbas.reporting.service.ConfigReader;
-import cz.cvut.kbss.inbas.reporting.service.data.DataLoader;
-import cz.cvut.kbss.inbas.reporting.util.ConfigParam;
-import cz.cvut.kbss.inbas.reporting.util.Constants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.Arrays;
-import java.util.Collections;
+public interface OptionsService {
 
-@Service
-public class OptionsService {
-
-    @Autowired
-    private ConfigReader configReader;
-
-    @Autowired
-    @Qualifier("localDataLoader")
-    private DataLoader localLoader;
-
-    @Autowired
-    @Qualifier("remoteDataLoader")
-    private DataLoader remoteLoader;
-
-    public Object getOptions(String type) {
-        switch (type) {
-            case "lvp":
-                return Arrays.asList(LowVisibilityProcedure.values());
-            case "occurrenceSeverity":
-                return Arrays.asList(OccurrenceSeverity.values());
-            case "location":
-                return new RawJson(localLoader.loadData("locations.json", Collections.emptyMap()));
-            case "operator":
-                return new RawJson(localLoader.loadData("operators.json", Collections.emptyMap()));
-            case "eventType":
-                return loadRemoteData(Constants.EVENT_TYPE_QUERY_FILE);
-            case "occurrenceCategory":
-                return loadRemoteData(Constants.OCCURRENCE_CATEGORY_QUERY_FILE);
-            default:
-                throw new IllegalArgumentException("Unsupported option type " + type);
-        }
-    }
-
-    private RawJson loadRemoteData(String queryFile) {
-        final String repositoryUrl = configReader.getConfig(ConfigParam.EVENT_TYPE_REPOSITORY_URL);
-        if (repositoryUrl == null) {
-            throw new IllegalStateException("Missing repository URL configuration.");
-        }
-        String query = localLoader.loadData(queryFile, Collections.emptyMap());
-        try {
-            query = URLEncoder.encode(query, Constants.UTF_8_ENCODING);
-            final String data = remoteLoader.loadData(repositoryUrl, Collections.singletonMap("query", query));
-            return new RawJson(data);
-        } catch (UnsupportedEncodingException e) {
-            throw new IllegalStateException("Unable to find encoding " + Constants.UTF_8_ENCODING, e);
-        }
-    }
+    /**
+     * Gets options of the specified type.
+     *
+     * @param type Options type
+     * @return Object representing the options. This will most often be a {@link RawJson} containing options retrieved
+     * from a remote repository. Otherwise, it can be an array/list of options.
+     * @throws IllegalArgumentException When the specified option type is not supported
+     */
+    Object getOptions(String type);
 }
--- inbas-reporting-tool/src/test/java/cz/cvut/kbss/inbas/reporting/environment/config/MockServiceConfig.java
+++ inbas-reporting-tool/src/test/java/cz/cvut/kbss/inbas/reporting/environment/config/MockServiceConfig.java
@@ -3,8 +3,8 @@ package cz.cvut.kbss.inbas.reporting.environment.config;
 import cz.cvut.kbss.inbas.reporting.service.*;
 import cz.cvut.kbss.inbas.reporting.service.data.FileDataLoader;
 import cz.cvut.kbss.inbas.reporting.service.data.RemoteDataLoader;
+import cz.cvut.kbss.inbas.reporting.service.options.OptionsService;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import static org.mockito.Mockito.mock;
 
 @Configuration
-@ComponentScan(basePackages = "cz.cvut.kbss.inbas.reporting.service.options")
 public class MockServiceConfig {
 
     @Bean
@@ -64,5 +63,10 @@ public class MockServiceConfig {
     @Bean
     public StatisticsService statisticsService() {
         return mock(StatisticsService.class);
+    }
+
+    @Bean
+    public OptionsService optionsService() {
+        return mock(OptionsService.class);
     }
 }
