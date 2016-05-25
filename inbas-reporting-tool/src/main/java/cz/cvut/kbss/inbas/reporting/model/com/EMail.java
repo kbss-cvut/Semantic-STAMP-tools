082911c2a1f2834746e01c900ab9b5b902849212
Bogdan Kostov <kostobog@fel.cvut.cz>
2016-05-25 19:33:48 +0200

    adding communication model and email

---
+++ inbas-reporting-tool/src/main/java/cz/cvut/kbss/inbas/reporting/model/com/EMail.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.kbss.inbas.reporting.model.com;
+
+import cz.cvut.kbss.datatools.mail.model.Message;
+import cz.cvut.kbss.inbas.reporting.model.OccurrenceReport;
+import cz.cvut.kbss.jopa.model.annotations.Id;
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import cz.cvut.kbss.jopa.model.annotations.OWLObjectProperty;
+import cz.cvut.kbss.jopa.model.annotations.Transient;
+import java.util.Set;
+
+/**
+ *
+ * @author Bogdan Kostov <bogdan.kostov@fel.cvut.cz>
+ */
+@OWLClass(iri = Vocabulary.Email)
+public class EMail {
+    
+    @Id(generated = true)
+    protected String uri;
+    
+    @Transient
+    protected Message message;
+    
+    @OWLDataProperty(iri = Vocabulary.hasId)
+    protected String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+    
+    
+}
