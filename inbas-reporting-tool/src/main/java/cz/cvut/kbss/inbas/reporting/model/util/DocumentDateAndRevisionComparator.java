d8a0464071bd07ad618a1eca27b480b9b2de9bba
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-05-27 16:48:20 +0200

    Prevent NPX in DocumentDateAndRevisionComparator.

--- inbas-reporting-tool/src/main/java/cz/cvut/kbss/inbas/reporting/model/util/DocumentDateAndRevisionComparator.java
+++ inbas-reporting-tool/src/main/java/cz/cvut/kbss/inbas/reporting/model/util/DocumentDateAndRevisionComparator.java
@@ -10,7 +10,16 @@ import java.util.Comparator;
 public class DocumentDateAndRevisionComparator implements Comparator<LogicalDocument> {
     @Override
     public int compare(LogicalDocument a, LogicalDocument b) {
-        int res = b.getDateCreated().compareTo(a.getDateCreated());
+        int res;
+        if (b.getDateCreated() == null && a.getDateCreated() == null) {
+            res = 0;
+        } else if (b.getDateCreated() == null && a.getDateCreated() != null) {
+            res = -1;
+        } else if (b.getDateCreated() != null && a.getDateCreated() == null) {
+            res = 1;
+        } else {
+            res = b.getDateCreated().compareTo(a.getDateCreated());
+        }
         if (res == 0) {
             res = b.getRevision().compareTo(a.getRevision());
         }
