d93a884fa9aea3c169eafc0f3b6c74c2465fd410
Bogdan Kostov <kostobog@fel.cvut.cz>
2016-06-20 17:27:11 +0200

    [fix] fixing starting and stopping of EmailSourceService

--- inbas-reporting-tool/src/main/java/cz/cvut/kbss/inbas/reporting/service/data/mail/EmailSourceService.java
+++ inbas-reporting-tool/src/main/java/cz/cvut/kbss/inbas/reporting/service/data/mail/EmailSourceService.java
@@ -1,6 +1,8 @@
 package cz.cvut.kbss.inbas.reporting.service.data.mail;
 
 import cz.cvut.kbss.datatools.mail.imap.idle.IDLEMailMessageReader;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import javax.mail.MessagingException;
 
@@ -12,13 +14,19 @@ public class EmailSourceService {
     @Autowired
     protected IDLEMailMessageReader idleImapReader;
 
-
-
+    /**
+     * WARNING: DO NOT CALL THIS BY HAND IN SPRING with configuration 
+     * cz.cvut.kbss.inbas.reporting.config.AppConfig. In this settings it is called 
+     * automatically. This method mya be called in a script outside.
+     */
+    @PostConstruct
     public void start() {
+        // FIXME : do not create a thread by hand find a better solution!!!
         Thread daemon = new Thread(() -> idleImapReader.waitForEmails());
         daemon.start();
     }
 
+    @PreDestroy
     public void stop() throws MessagingException {
         idleImapReader.close();
     }
