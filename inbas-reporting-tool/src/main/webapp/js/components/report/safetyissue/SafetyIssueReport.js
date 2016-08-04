8f3de267d490238aff389bfb75b4805769d4c625
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-08-04 16:39:08 +0200

    Working on safety issue report UI.

--- inbas-reporting-tool/src/main/webapp/js/components/report/safetyissue/SafetyIssueReport.js
+++ inbas-reporting-tool/src/main/webapp/js/components/report/safetyissue/SafetyIssueReport.js
@@ -1,17 +1,147 @@
 'use strict';
 
 var React = require('react');
+var Button = require('react-bootstrap').Button;
+var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
+var Panel = require('react-bootstrap').Panel;
+var assign = require('object-assign');
 
-var I18nMixin = require('../../../i18n/I18nMixin');
+var Actions = require('../../../actions/Actions');
+var CorrectiveMeasures = require('../../correctivemeasure/CorrectiveMeasures').default;
 var MessageMixin = require('../../mixin/MessageMixin');
+var I18nMixin = require('../../../i18n/I18nMixin');
+var injectIntl = require('../../../utils/injectIntl');
 var ReportDetailMixin = require('../../mixin/ReportDetailMixin');
+var ReportProvenance = require('../ReportProvenance').default;
+var ReportSummary = require('../ReportSummary').default;
+var ReportValidator = require('../../../validation/ReportValidator');
+var WizardGenerator = require('../../wizard/generator/WizardGenerator');
+var WizardWindow = require('../../wizard/WizardWindow');
 
 var SafetyIssueReport = React.createClass({
     mixins: [MessageMixin, I18nMixin, ReportDetailMixin],
 
+    propTypes: {
+        handlers: React.PropTypes.object,
+        report: React.PropTypes.object.isRequired,
+        loading: React.PropTypes.bool
+    },
+
+    getInitialState: function () {
+        return {
+            submitting: false,
+            loadingWizard: false,
+            isWizardOpen: false,
+            wizardProperties: null
+        };
+    },
+
+    onAttributeChange: function (attribute, value) {
+        var change = {};
+        change[attribute] = value;
+        this.onChanges(change);
+    },
+
+    onChanges: function (changes) {
+        this.props.handlers.onChange(changes);
+    },
+
+    onSave: function () {
+        var report = this.props.report;
+        this.onLoading();
+        // TODO factor graph
+        if (report.isNew) {
+            Actions.createReport(report, this.onSaveSuccess, this.onSaveError);
+        } else {
+            Actions.updateReport(report, this.onSaveSuccess, this.onSaveError);
+        }
+    },
+
+    onSubmit: function () {
+        this.onLoading();
+        Actions.submitReport(this.props.report, this.onSubmitSuccess, this.onSubmitError);
+    },
+
+
     render: function () {
-        return <div>Safety issue report</div>;
+        var report = this.props.report;
+
+        return <div>
+            <WizardWindow {...this.state.wizardProperties} show={this.state.isWizardOpen}
+                          onHide={this.closeSummaryWizard} enableForwardSkip={true}/>
+
+            <Panel header={this.renderHeader()} bsStyle='primary'>
+                <form>
+                    <div className='form-group'>
+                        <CorrectiveMeasures report={report} onChange={this.props.handlers.onChange}/>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-xs-12'>
+                            <ReportSummary report={report} onChange={this.onChange}/>
+                        </div>
+                    </div>
+
+                    <Panel>
+                        <ReportProvenance report={report} revisions={this.props.revisions}/>
+                    </Panel>
+
+                    {this.renderButtons()}
+                </form>
+            </Panel>
+            {this.renderMessage()}
+        </div>;
+    },
+
+    renderHeader: function () {
+        var fileNo = null;
+        if (this.props.report.fileNumber) {
+            fileNo =
+                <h3 className='panel-title pull-right'>{this.i18n('fileNo') + ' ' + this.props.report.fileNumber}</h3>;
+        }
+        return (
+            <div>
+                <h2 className='panel-title pull-left'>{this.i18n('safetyissuereport.title')}</h2>
+                {fileNo}
+                <div style={{clear: 'both'}}/>
+            </div>
+        )
+    },
+
+    renderButtons: function () {
+        if (this.props.readOnly) {
+            return this.renderReadOnlyButtons();
+        }
+        var loading = this.state.submitting,
+            saveDisabled = !ReportValidator.isValid(this.props.report) || loading,
+            saveLabel = this.i18n(loading ? 'detail.saving' : 'save');
+
+        return <ButtonToolbar className='float-right' style={{margin: '1em 0 0.5em 0'}}>
+            <Button bsStyle='success' bsSize='small' disabled={saveDisabled} title={this.getSaveButtonTitle()}
+                    onClick={this.onSave}>{saveLabel}</Button>
+            <Button bsStyle='link' bsSize='small' title={this.i18n('cancel-tooltip')}
+                    onClick={this.props.handlers.onCancel}>{this.i18n('cancel')}</Button>
+            {this.renderSubmitButton()}
+        </ButtonToolbar>;
+    },
+
+    getSaveButtonTitle: function () {
+        var titleProp = 'detail.save-tooltip';
+        if (this.state.submitting) {
+            titleProp = 'detail.saving';
+        } else if (!ReportValidator.isValid(this.props.report)) {
+            titleProp = ReportValidator.getValidationMessage(this.props.report);
+        }
+        return this.i18n(titleProp);
+    },
+
+    renderSubmitButton: function () {
+        return (
+            <Button bsStyle='primary' bsSize='small' title={this.i18n('detail.submit-tooltip')}
+                    onClick={this.onSubmit}>
+                {this.i18n('detail.submit')}
+            </Button>);
     }
 });
 
-module.exports = SafetyIssueReport;
+module.exports = injectIntl(SafetyIssueReport);
