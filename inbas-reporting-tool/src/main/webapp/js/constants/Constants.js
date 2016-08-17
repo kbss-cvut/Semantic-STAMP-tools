e912598f0057e4ab158a2577733e09b48c15c1c7
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-08-17 17:04:04 +0200

    Modified BI service URL to use the www.inbas.cz-based access to Pentaho.

--- inbas-reporting-tool/src/main/webapp/js/constants/Constants.js
+++ inbas-reporting-tool/src/main/webapp/js/constants/Constants.js
@@ -31,7 +31,7 @@ module.exports = {
     /**
      * URL of the remote BI solution providing statistics for the app
      */
-    STATISTICS_URL: 'https://147.32.84.201/pentaho/api/repos/%3Apublic%3AUCL%3ADashboard.wcdf/generatedContent',
+    STATISTICS_URL: 'https://www.inbas.cz/pentaho/api/repos/%3Apublic%3AUCL%3ADashboard.wcdf/generatedContent',
 
     /**
      * Sorting glyph icons
