9c4e863c3cee7dbb2fa1371ce876ed1e20094235
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-11-16 16:50:32 +0100

    [Feature #222] Added date of last phase modification attribute to CorrectiveMeasureRequest.

--- inbas-reporting-tool/src/test/java/cz/cvut/kbss/inbas/reporting/rest/dto/mapper/DtoMapperTest.java
+++ inbas-reporting-tool/src/test/java/cz/cvut/kbss/inbas/reporting/rest/dto/mapper/DtoMapperTest.java
@@ -79,6 +79,7 @@ public class DtoMapperTest {
         assertEquals(req.getUri(), dto.getUri());
         assertEquals(req.getDescription(), dto.getDescription());
         assertEquals(req.isImplemented(), dto.isImplemented());
+        assertEquals(req.getPhaseLastModified(), dto.getPhaseLastModified());
     }
 
     @Test
@@ -144,6 +145,7 @@ public class DtoMapperTest {
         final CorrectiveMeasureRequest request = new CorrectiveMeasureRequest();
         request.setUri(URI.create(Vocabulary.s_c_corrective_measure_request + "#req"));
         request.setDescription("Sample corrective measure.");
+        request.setPhaseLastModified(new Date());
         return request;
     }
 
