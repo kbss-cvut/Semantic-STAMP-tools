79040c411b4e52a981cc8d67e112ca832862d7f5
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2017-01-02 16:48:33 +0100

    Added tests for OrganizationDao.

--- inbas-reporting-tool/src/test/java/cz/cvut/kbss/reporting/persistence/dao/OrganizationDaoTest.java
+++ inbas-reporting-tool/src/test/java/cz/cvut/kbss/reporting/persistence/dao/OrganizationDaoTest.java
@@ -9,12 +9,14 @@ import static org.junit.Assert.*;
 
 public class OrganizationDaoTest extends BaseDaoTestRunner {
 
+    private static final String ORGANIZATION_NAME = "Czech Technical University in Prague";
+
     @Autowired
     private OrganizationDao organizationDao;
 
     @Test
     public void persistGeneratesOrganizationUri() {
-        final Organization organization = new Organization("Czech Technical University in Prague");
+        final Organization organization = new Organization(ORGANIZATION_NAME);
         assertNull(organization.getUri());
         organizationDao.persist(organization);
         assertNotNull(organization.getUri());
@@ -22,11 +24,24 @@ public class OrganizationDaoTest extends BaseDaoTestRunner {
 
     @Test
     public void testFindOrganizationByName() {
-        final Organization organization = new Organization("Czech Technical University in Prague");
+        final Organization organization = new Organization(ORGANIZATION_NAME);
         organizationDao.persist(organization);
 
         final Organization res = organizationDao.findByName(organization.getName());
         assertNotNull(res);
         assertEquals(organization.getUri(), res.getUri());
+    }
+
+    @Test
+    public void findByNameReturnsNullForUnknownName() {
+        final Organization organization = new Organization(ORGANIZATION_NAME);
+        organizationDao.persist(organization);
+
+        assertNull(organizationDao.findByName("unknownOrganization"));
+    }
+
+    @Test
+    public void findByNameReturnsNullForNullArgument() {
+        assertNull(organizationDao.findByName(null));
     }
 }
