119d9fe638c61d4c8addcc88bc00c63329f3eb31
Bogdan Kostov <kostobog@fel.cvut.cz>
2019-09-13 17:08:39 +0200

    fix imports

--- src/main/java/cz/cvut/kbss/datatools/xmlanalysis/common/jena/ExperimentGraphPattern.java
+++ src/main/java/cz/cvut/kbss/datatools/xmlanalysis/common/jena/ExperimentGraphPattern.java
@@ -5,37 +5,25 @@
  */
 package cz.cvut.kbss.datatools.xmlanalysis.common.jena;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import org.apache.jena.ext.com.google.common.collect.Iterators;
 import org.apache.jena.graph.Node;
-import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
-import org.apache.jena.graph.impl.TripleStore;
 import org.apache.jena.mem.GraphMem;
-import org.apache.jena.mem.GraphMemBase;
-import org.apache.jena.mem.GraphTripleStoreMem;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.QuerySolutionMap;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdf.model.impl.ModelCom;
-import org.apache.jena.rdf.model.impl.PropertyImpl;
-import org.apache.jena.shared.PrefixMapping;
-import org.apache.jena.sparql.syntax.Element;
 import org.apache.jena.util.iterator.ExtendedIterator;
 import org.apache.jena.util.iterator.SingletonIterator;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -110,7 +98,7 @@ public class ExperimentGraphPattern {
         
         // set template parameters according to the input
         QuerySolutionMap qs = new QuerySolutionMap();
-//        qs.add("person", ResourceFactory.createResource(personURI));
+//        qs.add("person", URIFactory.createResource(personURI));
         qs.add("className", ResourceFactory.createResource(className));
 //        qe.getQuery().getQueryPattern().
         qe.setInitialBinding(qs);
