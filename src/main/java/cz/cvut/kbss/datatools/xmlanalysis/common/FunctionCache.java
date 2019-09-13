206ac5dcfe97cd4e94a33583dac5f9e6ffecb8f1
Bogdan Kostov <kostobog@fel.cvut.cz>
2019-09-13 17:11:56 +0200

    experiment to handle definitions of keys of POJOs - not used

---
+++ src/main/java/cz/cvut/kbss/datatools/xmlanalysis/common/FunctionCache.java
@@ -0,0 +1,33 @@
+package cz.cvut.kbss.datatools.xmlanalysis.common;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Deprecated
+public class FunctionCache<T> {
+
+    protected Map<String, T> cache = new HashMap<>();
+
+    public String constructKey(String ... args){
+        return StringUtils.join(args, "-");
+    }
+
+    public T get(String ... args){
+        String key = constructKey(args);
+        return cache.get(key);
+    }
+
+    public T set(T val, String ... args){
+        String key = constructKey(args);
+        return cache.get(key);
+    }
+
+    public static void main(String[] args) {
+        FunctionCache<String> fc= new FunctionCache<>();
+        String key = fc.constructKey("a", "b", "c");
+        System.out.println(key);
+    }
+
+}
