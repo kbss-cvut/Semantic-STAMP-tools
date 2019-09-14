38058f322bb51030777211b7025e720d2cdcc6da
Bogdan Kostov <kostobog@fel.cvut.cz>
2019-09-14 13:51:45 +0200

    experiment - old code. Code is broken and commented.

---
+++ src/main/java/cz/cvut/kbss/datatools/xmlanalysis/experiments/lkpr/LKPRProcessModelAnalysis.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.kbss.datatools.xmlanalysis.experiments.lkpr;
+
+import cz.cvut.kbss.datatools.xmlanalysis.XMLAnalyzer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Deprecated
+public class LKPRProcessModelAnalysis {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(LKPRProcessModelAnalysis.class);
+    protected XMLAnalyzer xmlAnalyzer;
+    
+//    /**
+//     * This method analyzeXMLFile to generate report for the lkpr-process-models\aerobridge docking.xml
+//     */
+//    public void lkprAeroBridgeDockingProcessModelAnalysis(){
+//        xmlAnalyzer.analyzeXMLFile("lkpr-process-models/aerobridge docking.xml", "lkpr-process-models/output");
+//    }
+//
+//    /**
+//     * This method analyzeXMLFile to generate report for the lkpr-process-models\aerobridge go home.xml
+//     */
+//    public void lkprAeroBridgeGoHomeProcessModelAnalysis(){
+//        xmlAnalyzer.analyzeXMLFile("lkpr-process-models/aerobridge go home.xml", "lkpr-process-models/output");
+//    }
+//
+//    /**
+//     * This calls analyzeXMLFile to generate report for the lkpr-process-models\TEST 24_2.xml
+//     */
+//    public void lkprTEST_24_2ProcessModelAnalysis(){
+//        xmlAnalyzer.analyzeXMLFile("lkpr-process-models/TEST 24_2.xml", "lkpr-process-models/output");
+//    }
+//
+//    /**
+//     * This calss analyzeXMLFile to generate report for the lkpr-process-models\AandS included.xml
+//     */
+//    public void lkprAandS_IncludedProcessModelAnalysis(){
+//        xmlAnalyzer.analyzeXMLFile("lkpr-process-models/AandS included.xml", "lkpr-process-models/output");
+//    }
+//
+//    /**
+//     * This calss analyzeXMLFile to generate report for the lkpr-process-models\AandS included.xml
+//     */
+//    public void safetyTESTOnionStructureModelAnalysis(){
+//        xmlAnalyzer.analyzeXMLFile("lkpr-process-models/Safety TEST onion structure.xml", "lkpr-process-models/output");
+//    }
+
+
+
+    public void main(String[] args){
+
+//        lkprTEST_24_2ProcessModelAnalysis();
+//        lkprAandS_IncludedProcessModelAnalysis();
+//        
+//        lkprAeroBridgeDockingProcessModelAnalysis();
+//        lkprAeroBridgeGoHomeProcessModelAnalysis();
+//        safetyTESTOnionStructureModelAnalysis();
+    }
+}
