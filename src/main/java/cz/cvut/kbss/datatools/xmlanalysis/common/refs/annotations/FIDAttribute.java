3284d2f6a9bed795678c12dc77ae92ec06908a19
Bogdan Kostov <kostobog@fel.cvut.cz>
2019-09-20 23:32:00 +0200

    object reference framework - add/update javadoc

--- src/main/java/cz/cvut/kbss/datatools/xmlanalysis/common/refs/annotations/FIDAttribute.java
+++ src/main/java/cz/cvut/kbss/datatools/xmlanalysis/common/refs/annotations/FIDAttribute.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME) @Target({ElementType.FIELD}) // TODO - implement ElementType.METHOD to use for getters and setters instead of the field
 public @interface FIDAttribute {
-    /** the id/name of the foreign-key - key mapping. Not required when there is only one key. Must be used in case of multiple keys. If a key */
+    /** the id/name of the key mapping. Not required when there is only one key-mapping. Must be used in case of multiple key-mappings.*/
     String value() default Constants.NO_ID_NAME;
 //    String fkId() default Constants.NO_ID_NAME; // NOT IMPLEMENTED // This is used when a class contains multiple sets of fields defining FKs to the same referenced class.
     /** The name of the field */
