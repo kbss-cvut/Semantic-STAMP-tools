b41138336f67172554393da5e82962e3f9463206
Bogdan Kostov <kostobog@fel.cvut.cz>
2019-11-07 04:23:00 +0100

    Feature #995 - Event chain from selected process - client side   - loadingProcessFlow from Server   - adding chain of events from process flow, ordered on the time line

--- inbas-reporting-tool/src/main/webapp/js/actions/Actions.js
+++ inbas-reporting-tool/src/main/webapp/js/actions/Actions.js
@@ -9,7 +9,7 @@ module.exports = Reflux.createActions([
     'phaseTransition',
     'loadRevisions', 'loadReport',
 
-    'loadOptions',
+    'loadOptions', 'loadProcessFlow',
 
     'importSchema',
 
