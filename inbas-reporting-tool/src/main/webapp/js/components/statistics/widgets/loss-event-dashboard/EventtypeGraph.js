b9d099c10f60a55852882c3364236c0bd1e2725e
Bogdan Kostov <kostobog@fel.cvut.cz>
2020-06-29 23:07:34 +0200

    fix statistics - remove SERVICE fetching labels from factor graph query. Add legend for process colors.

--- inbas-reporting-tool/src/main/webapp/js/components/statistics/widgets/loss-event-dashboard/EventtypeGraph.js
+++ inbas-reporting-tool/src/main/webapp/js/components/statistics/widgets/loss-event-dashboard/EventtypeGraph.js
@@ -1,4 +1,5 @@
 'use strict';
+
 import React from "react";
 import Graph from "react-graph-vis";
 
@@ -8,22 +9,46 @@ import Utils from "../../Utils";
 import GraphUtils from "./GraphUtils";
 import LoadingWrapper from "../../../misc/hoc/LoadingWrapper";
 import GraphOptions from "./GraphOptions";
+import Vocabulary from "../../../../constants/Vocabulary";
+import Actions from "../../../../actions/Actions";
+import Constants from "../../../../constants/Constants";
+import OptionsStore from "../../../../stores/OptionsStore";
 
 class EventtypeGraph extends React.Component {
 
     constructor(props) {
         super(props);
+        this.unsubscribe = OptionsStore.listen(this._onOptionsLoaded);
+        Actions.loadOptions(Constants.OPTIONS.EVENT_TYPE);
+        Actions.loadOptions(Constants.OPTIONS.FACTOR_TYPE);
         this.state = {
             data: {},
+            eventTypes: null,
+            factorTypes: null,
+            labelMap: null,
+            nodeColorMap: null,
             graphOptions: GraphOptions
         }
+    }
+
+    _onOptionsLoaded = (type, options) => {
+        let unsub = false;
+        if(type === Constants.OPTIONS.EVENT_TYPE && options){
+            unsub = this.state.factorTypes;
+            this.setState({eventTypes: options});
+        }else if(type === Constants.OPTIONS.FACTOR_TYPE){
+            unsub = this.state.eventTypes;
+            this.setState({factorTypes: options});
+        }
+        if(unsub)
+            this.unsubscribe();
     }
 
      load() {
         if (this.props.eventTypeGraph) {
             const rowsAll = Utils.sparql2table(this.props.eventTypeGraph);
             // perform group by
-            let groupCols = ['event_type', 'factor_type', 'relation_type', 'process_event_label', 'process_factor_label'];
+            let groupCols = ['event_type', 'factor_type', 'relation_type', 'process_event_type', 'process_factor_type'];
             let groups = rowsAll.reduce(function(grouped, row){
                 let key = '';
                 let p = {};
@@ -44,50 +69,64 @@ class EventtypeGraph extends React.Component {
             //   ?event_type ?relation_type ?factor_type (COUNT(*) AS ?count)
             let nodeColorMap = [];
             [...new Set(
-                rows.map(x => x.process_event_label)
-                    .concat(rows.map(x => x.process_factor_label))
-                    .filter(x => x))
+                rows.map(x => x.process_event_type)
+                    .concat(rows.map(x => x.process_factor_type)))
+                    // .filter(x => x))
             ].forEach((val, i, arr) => nodeColorMap[val] = 'hsl(' + i*(360./arr.length) + ', 50%, 50%)');//"#ff0000"
 
+            let labelMap = new Map();
+            [this.state.eventTypes, this.state.factorTypes].forEach(voc => voc.forEach(
+                e =>
+                    labelMap.set(
+                        e['@id'],
+                        e[Vocabulary.RDFS_LABEL] ?
+                            (e[Vocabulary.RDFS_LABEL]['@value'] ? e[Vocabulary.RDFS_LABEL]['@value'] : e[Vocabulary.RDFS_LABEL])
+                            : e['@id']
+                    )
+                )
+            );
 
             let nodes = []
             let edges = []
-
+            let legend = {};
             let maxNodeHolder = {value:1}
 
             let fromToCount={};
             rows.forEach((item) => {
                 let ft,et;
                 if (GraphUtils.validEventType(item.factor_type)) {
-                    ft = GraphUtils.addNode(item.factor_type, nodes, maxNodeHolder)
-                    nodes[ft - 1].color = nodeColorMap[item.process_factor_label];
+                    ft = GraphUtils.addNode(labelMap.get(item.factor_type), nodes, maxNodeHolder)
+                    nodes[ft - 1].process = item.process_factor_type;
                 }
 
                 if (GraphUtils.validEventType(item.event_type)) {
-                    et = GraphUtils.addNode(item.event_type, nodes, maxNodeHolder);
-                    nodes[et - 1].color = nodeColorMap[item.process_event_label];
+                    et = GraphUtils.addNode(labelMap.get(item.event_type), nodes, maxNodeHolder);
+                    nodes[et - 1].process = item.process_event_type;
                 }
 
                 if (et && ft) {
-                    ft = GraphUtils.addNode(item.factor_type, nodes,maxNodeHolder);
-                    if(item.process_factor_label)
-                        nodes[ft - 1].title = nodes[ft - 1].title + " in " + item.process_factor_label;
-                    et = GraphUtils.addNode(item.event_type, nodes);
-                    if(item.process_event_label)
-                        nodes[et - 1].title = nodes[et - 1].title + " in " + item.process_event_label;
-                    GraphUtils.addEdge(ft, et, item.relation_type, item.count, fromToCount, edges,maxNodeHolder);
+                    GraphUtils.addEdge(ft, et, labelMap.get(item.relation_type), item.count, fromToCount, edges,maxNodeHolder);
                 }
             });
+            nodes.forEach(n => {
+                n.color = nodeColorMap[n.process];
+                let l = labelMap.get(n.process);
+                l = l ? l : n.process;
+                if(l)
+                    n.title = n.title + " in " + l;
+                legend[l] = n.color;
+            });
+
 
             // let nodeFiltered = [];
             // edges = edges.filter((edge) => { if ( edge.value > 5 ) {nodeFiltered.push(edge.from); nodeFiltered.push(edge.to); return true} else return false})
             // nodes = nodes.filter((node) => { return nodeFiltered.indexOf(node.id) >= 0});
 
-            return {nodes, edges};
+            return {nodes, edges, legend};
         } else {
             return null;
         }
-    };
+    }
 
     render() {
         const data = this.load();
@@ -98,8 +137,24 @@ class EventtypeGraph extends React.Component {
             return <div>
                 <h4 className="content-center">{this.props.title}</h4>
                 <Graph graph={data} options={this.state.graphOptions} style={{ width : '100%', height:'625px'}}/>
+
+                {this._renderGraphLegend(data.legend)}
             </div>;
         }
+    }
+
+    _renderGraphLegend(legend){
+        return <div><h4 className="content-center">Legend</h4>
+            <table style={{float: 'right'}}>
+                {Object.keys(legend).sort().map(l =>
+                    <tr>
+                        <th style={{background:legend[l], width:'10px'}}></th>
+                        <th>{l}</th>
+                    </tr>
+                )
+                }
+            </table>
+        </div>
     }
 }
 
